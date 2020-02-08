c6d03c73329f2a5c267ed4fc347f1ebb79a7bff6
Mike <mikefelix@gmail.com>
2020-02-08 14:49:27 -0500

    Normalize fare types during NJT and LIRR imports

--- src/services/importUtils/utils.js
+++ src/services/importUtils/utils.js
@@ -1,6 +1,24 @@
 const fs = require("fs");
 const csv = require("fast-csv");
 
+const normalizeFare = fare => {
+  // delete any asterisks and make number
+  return parseFloat(fare.replace("*", "").trim());
+};
+
+const normalizeFareType = fareType => {
+  const type = fareType.trim().toLowerCase();
+  if (["one-way", "peak one-way"].includes(type)) { return "one-way-peak"; }
+  if (["off-peak one-way"].includes(type)) { return "one-way-off-peak"; }
+  if (["sr. cit/disabled/m’care one-way", "one-way reduced"].includes(type)) { return "one-way-reduced"; }
+  if (["10-trip", "peak ten-trip"].includes(type)) { return "10-trip-peak"; }
+  if (["off-peak ten-trip"].includes(type)) { return "10-trip-off-peak"; }
+  if (["on-board peak one-way"].includes(type)) { return "one-way-peak-on-board"; }
+  if (["on-board off-peak one-way"].includes(type)) { return "one-way-off-peak-on-board"; }
+  if (["one-way child"].includes(type)) { return "one-way-child"; }
+  return type;
+};
+
 const readCSV = file =>
 new Promise((resolve, reject) => {
   try {
@@ -18,5 +36,7 @@ new Promise((resolve, reject) => {
 });
 
 module.exports = {
+  normalizeFare,
+  normalizeFareType,
   readCSV
 }
