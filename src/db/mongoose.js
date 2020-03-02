8cd906070a6648f3630cb9023db41cba9af2b278
Mike <mikefelix@gmail.com>
2020-03-02 07:54:38 -0500

    Move from Mongo Atlas to mLab

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 require('dotenv').config();
 
-const URI = process.env.NODE_ENV === "production" ? process.env.MONGO_URI_PROD : process.env.MONGO_URI_TEST;
+const URI = process.env.NODE_ENV === "production" ? process.env.MONGODB_URI : process.env.MONGO_URI_TEST;
 
 mongoose.connect(URI, {
   useNewUrlParser: true,
