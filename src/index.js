837a9d182e294673b1cee5c38b56114259741f58
Mike <mikefelix@gmail.com>
2020-05-15 06:34:49 -0400

    Finish CRUD routes for query model

--- src/index.js
+++ src/index.js
@@ -5,6 +5,10 @@ const importRoutes = require("./routes/importRoutes");
 const stationRoutes = require("./routes/stationRoutes");
 const queryRoutes = require("./routes/queryRoutes");
 
+const dbInUse = process.env.NODE_ENV === "production"
+  ? "production"
+  : "dev";
+
 try {
   const port = process.env.PORT || 3000;
   const app = express();
@@ -13,7 +17,7 @@ try {
   app.use(importRoutes);
   app.use(stationRoutes);
   app.use(queryRoutes);
-  app.listen(port, () => console.log(`Server running on port ${port}...`));
+  app.listen(port, () => console.log(`Server running on port ${port} using ${dbInUse} database.`));
 } catch (err) {
   console.log("Error from index.js", err);
 }
--- src/routes/queryRoutes.js
+++ src/routes/queryRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-// const Query = require("../models/query");
+const Query = require("../models/query");
 const { getRecommendation } = require("../services/recommendationService");
 
 const router = new express.Router();
@@ -9,33 +9,55 @@ router.post("/query", async (req, res) => {
     const userQuery = req.body;
     const result = await getRecommendation(userQuery);
     if (result instanceof Error) {
-      res.status(400).send(`Error: ${result.message}`);
+      return res.status(400).send(`Error: ${result.message}`);
     } else {
-      res.send(result);
+      return res.send(result);
     }
   } catch (err) {
     res.status(400).send(err);
   }
 });
 
-// router.get("/queries", async (req, res) => {
-//   try {
-//     const queries = await Query.find({});
-//     if (!queries) return res.status(404).send(`No queries found.`);
-//     res.send(queries);
-//   } catch (err) {
-//     res.status(500).send(err);
-//   }
-// });
+router.get("/query", async (req, res) => {
+  try {
+    const queries = await Query.find({});
+    if (!queries) return res.status(404).send(`No queries found.`);
+    return res.send(queries);
+  } catch (err) {
+    return res.status(500).send(err);
+  }
+});
 
-// router.get("/queries/:id", async (req, res) => {
-//   try {
-//     const query = await Query.findById(req.params.id);
-//     if (!query) return res.status(404).send(`Query not found with id ${req.params.id}.`);
-//     res.send(query);
-//   } catch (err) {
-//     res.status(500).send(err);
-//   }
-// });
+router.get("/query/:id", async (req, res) => {
+  try {
+    const query = await Query.findById(req.params.id);
+    if (!query) return res.status(404).send(`Query not found with id ${req.params.id}.`);
+    return res.send(query);
+  } catch (err) {
+    return res.status(500).send(err);
+  }
+});
+
+router.put("/query/:id", async (req, res) => {
+  try {
+    const query = await Query.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    if (!query) return res.status(404).send(`Query not found with id ${req.params.id}.`);
+    return res.send(query);
+  } catch (err) {
+    return res.status(500).send(err);
+  }
+});
+
+router.delete("/query/:id", async (req, res) => {
+  try {
+    const deleted = await Query.findOneAndDelete({
+      _id: req.params.id
+    });
+    if (!deleted) return res.status(404).send(`Query not found with id ${req.params.id}.`);
+    return res.status(204).send();
+  } catch (err) {
+    return res.status(500).send(err);
+  }
+});
 
 module.exports = router;
