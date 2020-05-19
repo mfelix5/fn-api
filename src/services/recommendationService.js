8e9664c798e033e956386baa10dc7008a1e56c41
Mike <mikefelix@gmail.com>
2020-05-19 15:37:09 -0400

    Add savings to response when recommendation is monthly, remove on-hand logic

--- src/services/recommendationService.js
+++ src/services/recommendationService.js
@@ -7,7 +7,7 @@ const getRecommendation = async (travelData) => {
       if (!travelData[field]) throw new Error(`${field} is required.`);
     });
 
-    const { destination, oneWaysNeeded, onHand, originId } = travelData;
+    const { destination, oneWaysNeeded, originId } = travelData;
     const station = await stationService.findStationById(originId);
     if (!station) throw new Error(`Unable to find station.`);
     const { destinations } = station;
@@ -22,32 +22,6 @@ const getRecommendation = async (travelData) => {
     const recommendation = {};
     const leastExpensiveOptions = {};
     const leastExpensiveCosts = {};
-
-    if (onHand) {
-      const totalOneWaysNeeded = Object.values(oneWaysNeeded).reduce((a, b) => a + b);
-      if (onHand.oneWay >= totalOneWaysNeeded) {
-        recommendation.use = oneWaysNeeded;
-        recommendation.message = "One ways on hand are sufficient for your planned travel.";
-      }
-
-      while (onHand.oneWay > 0) {
-        // identify week with lowest non-zero travel
-        let minWeek;
-        Object.keys(oneWaysNeeded).forEach((week) => {
-          if (oneWaysNeeded[week] > 0) {
-            minWeek = (minWeek === undefined || oneWaysNeeded[week] < oneWaysNeeded[minWeek]) ? week : minWeek;
-          }
-        });
-        // apply on hand tickets to minWeek
-        if (oneWaysNeeded[minWeek] >= onHand.oneWay) {
-          oneWaysNeeded[minWeek] -= onHand.oneWay;
-          onHand.oneWay = 0;
-        } else {
-          onHand.oneWay -= oneWaysNeeded[minWeek];
-          oneWaysNeeded[minWeek] = 0;
-        }
-      }
-    }
 
     Object.keys(oneWaysNeeded).forEach((week) => {
       if (oneWaysNeeded[week] > 0) {
@@ -81,9 +55,8 @@ const getRecommendation = async (travelData) => {
       recommendation.message = "A combination of one way and weekly tickets is your least expensive option.";
     } else {
       purchase.monthly = 1;
-      Object.keys(oneWaysNeeded).forEach((week) => {
-        use[week] = "monthly";
-      });
+      Object.keys(oneWaysNeeded).forEach((week) => { use[week] = "monthly" });
+      recommendation.savings = costOfLeastExpensiveOptions - fares["monthly"];
       recommendation.totalCost = fares["monthly"];
       recommendation.message = "A monthly ticket is your least expensive option.";
     }
@@ -91,7 +64,7 @@ const getRecommendation = async (travelData) => {
     recommendation.purchase = purchase;
     recommendation.use = use;
     const queryObj = { origin: station.name, ...travelData, recommendation, fares };
-    const result = await module.exports.saveQuery(queryObj); // Calling saveQuery via module.exports so that testing stub works correctly. There must be a better way.
+    const result = await module.exports.saveQuery(queryObj); // FIXME: Calling saveQuery via module.exports so that testing stub works correctly. There must be a better way.
     return result;
   } catch (error) {
     return error;
